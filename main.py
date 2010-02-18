9eec141ea38ce5716e7e8c8cfc890db9435961e8
Adam <adam@Thurloat-6.local>
2010-02-17 20:38:25 -0400

    Adding Cherrypy

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import cherrypy
+import wsgiref.handlers
 
+class PathRoot:
+    @cherrypy.expose
+    def index(self):
+        return "This is the HTTPDB Server."
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-
-
-class MainHandler(webapp.RequestHandler):
-
-  def get(self):
-    self.response.out.write('Hello HTTPDB!')
-
+class PathGet:
+    @cherrypy.expose
+    def index(self):
+        return "{'key':'value','http':'db'}"
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
-  util.run_wsgi_app(application)
-
+  root = PathRoot()
+  root.get = PathGet()
+  app = cherrypy.tree.mount(root,"/")
+  wsgiref.handlers.CGIHandler().run(app)
 
 if __name__ == '__main__':
   main()
