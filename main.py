909233b5a5ee48583fd6cb61c22d0dcd2dc40a65
Adam <adam@Thurloat-6.local>
2010-02-18 00:30:40 -0400

    adding get and set initial functions that interact with the raw appengine datastore

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 import cherrypy
+import simplejson
 import wsgiref.handlers
+from google.appengine.api import datastore
+from google.appengine.api import datastore_types
+
+class KeyValue(datastore.Entity):
+    def __init__(self,key,value=None):
+        datastore.Entity.__init__(self,'Entity',name=key)
+        self[key] = value
 
 class PathRoot:
     @cherrypy.expose
     def index(self):
         return "This is the HTTPDB Server."
-
-class PathGet:
     @cherrypy.expose
-    def index(self):
-        return "{'key':'value','http':'db'}"
+    def set(self, getValue=None, setValue=None):
+        try:
+            entity = KeyValue(getValue,setValue)
+            entity.value = setValue
+            datastore.Put(entity)
+        except:
+            return "FAIL"
+        return "SET"
+
+    @cherrypy.expose
+    def get(self, getValue=None):
+        key = datastore_types.Key.from_path("Entity",getValue)
+        try:
+            e = datastore.Get(key)
+        except:
+            return "{}"
+        return simplejson.dumps(e)
 
 def main():
-  root = PathRoot()
-  root.get = PathGet()
-  app = cherrypy.tree.mount(root,"/")
-  wsgiref.handlers.CGIHandler().run(app)
+    """
+    Creating a cherrypy app using the PathRoot class as a root url handler.
+    index, set & get are exposed to urls.
+    """
+    wsgiref.handlers.CGIHandler().run(cherrypy.tree.mount(PathRoot(),"/"))
 
 if __name__ == '__main__':
   main()
